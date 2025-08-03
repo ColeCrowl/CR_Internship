00dc602f98ca05fe6b4606e55f38642dd3c9d835
Cole Crowl <78942632+ColeCrowl@users.noreply.github.com>
2025-08-03 12:49:16 -0600

    Create WN11-AU-000505.ps1

---
+++ STIG/WIN-AU-0005 Series/WN11-AU-000505.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures The Security Event Log size must be configured to 1024000 KB (1 GB) or greater
+
+.NOTES
+    Author          : Cole Crowl
+    LinkedIn        : https://www.linkedin.com/in/colecrowl/
+    GitHub          : https://github.com/ColeCrowl/ColeCrowl
+    Date Created    : 2025-08-03
+    Last Modified   : 2025-08-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000505
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-03
+    Tested By       : Cole Crowl
+    Systems Tested  : Windows 10 Pro 6.3
+    PowerShell Ver. :  5.1.26100.4652
+
+.USAGE
+    .USAGE
+    This script remediates the 'WN11-AU-000505' STIG by configuring the 
+    specified settings. It should be run with administrative privileges.
+
+    Example syntax:
+    PS C:\> .\STIG-WN11-AU-000505_fix.ps1
+#>
+
+$sizeBytes = 1024000 * 1024
+wevtutil sl Security /ms:$sizeBytes
