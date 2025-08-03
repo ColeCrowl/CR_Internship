f7d7a41075f3c42319feffc645399c494be14cb2
Cole Crowl <78942632+ColeCrowl@users.noreply.github.com>
2025-08-03 14:09:54 -0600

    Create WN11-AU-000570.ps1

---
+++ STIG/WIN-AU-0005 Series/WN11-AU-000570.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures Windows 11 must be configured to audit Detailed File Share Failures.
+
+.NOTES
+    Author          : Cole Crowl
+    LinkedIn        : https://www.linkedin.com/in/colecrowl/
+    GitHub          : https://github.com/ColeCrowl/ColeCrowl
+    Date Created    : 2025-08-03
+    Last Modified   : 2025-08-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000570
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-03
+    Tested By       : Cole Crowl
+    Systems Tested  : Windows 10 Pro 6.3
+    PowerShell Ver. :  5.1.26100.4652 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000570).ps1 
+#>
+
+# Define the audit policy subcategory
+$subcategory = "Detailed File Share"
+
+# Define the audit flag for failures
+$failureFlag = "/failure:enable"
+
+# Use auditpol.exe to set the policy
+auditpol /set /subcategory:"$subcategory" $failureFlag
