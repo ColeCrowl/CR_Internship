585c9fd35e0fe9a8dd6bca9847bfb0357aad25dd
Cole Crowl <78942632+ColeCrowl@users.noreply.github.com>
2025-08-11 23:05:24 -0600

    Update WN11-CC-000038 .ps1

--- STIG/WN11-CC Series/WN11-CC-000038 .ps1
+++ STIG/WN11-CC Series/WN11-CC-000038 .ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables a legacy authentication method that can store credentials in an insecure manner.
+
+.NOTES
+    Author          : Cole Crowl
+    LinkedIn        : https://www.linkedin.com/in/colecrowl/
+    GitHub          : https://github.com/ColeCrowl/ColeCrowl
+    Date Created    : 2025-08-11
+    Last Modified   : 2025-08-11
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000038
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-11
+    Tested By       : Cole Crowl
+    Systems Tested  : Windows 10 Pro 6.3
+    PowerShell Ver. :  5.1.26100.4652 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000038).ps1 
+#>
+
 # Define the registry path for WDigest
 $wDigestPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest"
 
